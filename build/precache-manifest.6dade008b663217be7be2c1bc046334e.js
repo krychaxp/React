4aa4e5ceeb6b9fbba7fd8a711d8111259c02c6fe
krychaxp <krychaxp@op.pl>
2020-06-19 01:15:50 +0200

    Add build folder

---
+++ build/precache-manifest.6dade008b663217be7be2c1bc046334e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cce6dbe85765a7d52d9acb7880d9918",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59b5c1de10a45de501b2",
+    "url": "/static/css/main.5457295b.chunk.css"
+  },
+  {
+    "revision": "b389bfbfa91695162648",
+    "url": "/static/js/2.cdf6a8a2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.cdf6a8a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59b5c1de10a45de501b2",
+    "url": "/static/js/main.a0ccbdb4.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
