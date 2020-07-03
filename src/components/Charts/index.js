8505d2c7bfa2df750e778ff6c3eff123472c98c9
krychaxp <krychaxp@op.pl>
2020-07-03 12:02:04 +0200

    change all gituhub-searcher, add charts and change routing

---
+++ src/components/Charts/index.js
@@ -0,0 +1,76 @@
+import React,{useEffect} from "react";
+import * as Highcharts from 'highcharts';
+import * as Exporting from 'highcharts/modules/exporting';
+import axios from 'axios'
+export default () => {
+    useEffect(() => {
+        console.log("1")
+        return () => {
+            console.log("2")
+        }
+    }, [])
+    Exporting(Highcharts);
+    (async function() {
+        const {data} = await axios.get("http://cfg.krychaxp.pl/data.php?json=pm")
+        Highcharts.chart('chart', {
+            title: {
+                text: 'Zanieczyszczenie powietrza w Poznaniu',
+                style: {
+                    fontSize: '25px'
+                }
+            },
+
+            subtitle: {
+                text: `Ostatnie ${data.length} zarejestrowanych pomiarów`
+            },
+
+            yAxis: {
+                title: {
+                    text: 'x µg/m<sup>3</sup>',
+                    useHTML: true
+                }
+            },
+
+            xAxis: [{
+                categories: data.map((v, i) => `${v.date}<br>${v.time}`),
+                crosshair: true
+            }],
+
+            legend: {
+                layout: 'horizontal',
+                align: 'center',
+                verticalAlign: 'top',
+            },
+
+            plotOptions: {
+                series: {
+                    label: {
+                        connectorAllowed: false
+                    }
+                }
+            },
+            tooltip: {
+                shared: true,
+                valueSuffix: '{value} µg/m<sup>3</sup>',
+                useHTML: true
+            },
+            series: [{
+                name: 'PM1',
+                data: data.map(v => +v.pm1)
+            }, {
+                name: 'PM10',
+                data: data.map(v => +v.pm10)
+            }, {
+                name: 'PM25',
+                data: data.map(v => +v.pm25)
+            }]
+        });
+    })();
+
+
+    return (
+        <div id="chart">
+            charts
+        </div>
+    )
+}
